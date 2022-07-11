65a1781ee7e3c7936df4293f01455b843b2fc278
denis <denismatysik@gmail.com>
2022-07-11 14:50:20 +0300

    add

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const fileInput = document.querySelector(".file-input");
 const container = document.querySelector(".container");
 const choseImgBtn = document.querySelector(".chose-img");
 const previewImg = document.querySelector(".previews-img img");
+const filterOptions = document.querySelectorAll(".filter button");
 
 const loadImage = ()=>{
     let file = fileInput.files[0]; // get user select file
@@ -11,7 +12,13 @@ const loadImage = ()=>{
         container.classList.remove("disable");
     })
 }
+fileInput.addEventListener("change", loadImage);
+choseImgBtn.addEventListener("click", ()=> fileInput.click());
 
-fileInput.addEventListener("change", loadImage)
-
-choseImgBtn.addEventListener("click", ()=> fileInput.click())
+filterOptions.forEach(option => {
+    option.addEventListener("click", ()=>{
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+        console.log(option);
+    });
+})
